2967732035c62de83dcf4ed3556bf13d6b5c09f1
ZmeykaTatiana <zacharovanaja@mail.ru>
2023-03-01 17:23:00 +0300

    making two task by Alex. Count of wotds in name. count of symbols in name, one field shouln't be empty

--- src/main/java/by/zmeyka/LessonProject/model/User.java
+++ src/main/java/by/zmeyka/LessonProject/model/User.java
@@ -1,9 +1,11 @@
 package by.zmeyka.LessonProject.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Data
 @AllArgsConstructor
@@ -16,9 +18,24 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column(name="user_name")
+    @NotEmpty(message="name can't be empty")
+
     private String userName;
     @Column(name="user_lastname")
+
     private String userLastName;
     @Column(name="user_email")
+
     private String email;
+
+
+    @ManyToOne
+    @JoinColumn(name="office_id")
+    private Office office;
+
+    public User(String userName, String userLastName, String email) {
+        this.userName = userName;
+        this.userLastName = userLastName;
+        this.email = email;
+    }
 }
